6ad1f9ee1b34d62a658d26f56f65664c9c1f5c5d
Javier Maldonado <3goliad@gmail.com>
2016-12-23 20:09:18 -0500

    moved to sdl2

--- src/indy.cpp
+++ src/indy.cpp
@@ -1,37 +1,67 @@
+#include "SDL2/SDL_image.h"
+#include "SDL2/SDL.h"
 #include <iostream>
-#include <SDL/SDL.h>
+#ifdef __EMSCRIPTEN__
+#include <emscripten/emscripten.h>
+#endif
+#pragma clang diagnostic ignored "-Wconstant-conversion"
 #include "image_loader.h"
+#pragma clang diagnostic pop
+
+SDL_Window *window;
+SDL_Surface *screen;
+SDL_Surface *hello;
+
+void display_once() {
+	std::clog << "Running main loop\n";
+  SDL_BlitSurface(hello, NULL, screen, NULL);
+   SDL_UpdateWindowSurface( window );
+}
 
 int main() {
-  SDL_Surface* hello = NULL;
-  SDL_Surface* screen = NULL;
+	window = NULL;
+  screen = NULL;
+  hello = NULL;
 
   std::clog << "Initializing SDL\n";
-  if ((SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) == -1)) {
-	  std::cerr << "Could not initialize SDL\n";
-	  std::cerr << SDL_GetError();
-          exit(-1);
-    }
+  if ((SDL_Init(SDL_INIT_VIDEO) == -1)) {
+    std::cerr << "Could not initialize SDL\n";
+    std::cerr << SDL_GetError();
+    exit(-1);
+  }
 
   atexit(SDL_Quit);
 
-  screen = SDL_SetVideoMode(800, 600, 32, SDL_SWSURFACE);
-  if (screen == NULL ) {
-	  std::cerr << "Could not get video mode\n";
-	  std::cerr << SDL_GetError();
-	  exit(-1);
+  std::clog << "Trying to set video mode\n";
+  window = SDL_CreateWindow( "DIAMOND", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+  if (window == NULL) {
+    std::cerr << "Could not get video mode\n";
+    std::cerr << SDL_GetError();
+    exit(-1);
   }
 
-  hello = load_image("assets/alpine.jpg");
-  if(hello == 0) {
-	  std::cerr << "Could not load asset\n";
-	  exit(-1);
-  }
+  screen = SDL_GetWindowSurface(window);
 
-  SDL_BlitSurface(hello, NULL, screen, NULL);
-  SDL_Flip(screen);
-  SDL_Delay(2000);
+  std::clog << "Trying to load hello image\n";
+  hello = IMG_Load("assets/alpine.jpg");
+  if (hello == NULL) {
+    std::cerr << "Could not load asset\n";
+    exit(-1);
+  }
+  std::clog << "Should have loaded image\n";
+
+#ifdef __EMSCRIPTEN__
+  emscripten_set_main_loop(display_once, 0, 1);
+#else
+  while (true) {
+    display_once();
+    // Delay to keep frame rate constant (using SDL)
+    SDL_Delay(2000);
+    break;
+  }
+#endif
   SDL_FreeSurface(hello);
+  SDL_DestroyWindow(window);
 
   std::clog << "Quitting SDL\n";
   SDL_Quit();
