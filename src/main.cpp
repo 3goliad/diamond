0719d23a8c90ab2f8d15a8c6867667ad8243d339
Javier Maldonado <3goliad@gmail.com>
2017-01-04 13:41:23 -0500

    working build with glut and glew

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
 #include <cstdlib>
 #include <iostream>
 
-#include <SDL2/SDL.h>
 #include <GL/glew.h>
-#include <SDL2/SDL_opengl.h>
+#include <GL/glut.h>
 #ifdef __EMSCRIPTEN__
 #include <emscripten/emscripten.h>
 #endif
@@ -11,67 +10,17 @@
 
 #include "shaders.hpp"
 
+GLuint shaderProgram;
+
 // Window dimensions
 const GLuint SCREEN_WIDTH = 800;
 const GLuint SCREEN_HEIGHT = 600;
 
-//grabs current error and prints it
-void sdlErr(int line = -1) {
-  	std::string error = SDL_GetError();
-    if (error != "") {
-      std::cout << "SDL Error : " << error << std::endl;
-
-      if (line != -1) {
-        std::cout << "\nLine : " << line << std::endl;
-      }
-		SDL_ClearError();
-  }
+void reshape(int x, int y) {
+  glViewport(0, 0, x, y);
 }
 
-
-void initSDL(SDL_Window **win, SDL_GLContext *context) {
-  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-    sdlErr(__LINE__);
-    exit(EXIT_FAILURE);
-  }
-
-  //set opengl to only use the core functions
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-
-  *win = SDL_CreateWindow("diamond", SDL_WINDOWPOS_UNDEFINED,
-                                     SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                                     SCREEN_HEIGHT, SDL_WINDOW_OPENGL| SDL_WINDOW_SHOWN);
-  sdlErr(__LINE__);
-  if (*win == nullptr) {
-    SDL_Quit();
-    exit(EXIT_FAILURE);
-  }
-
-  *context = SDL_GL_CreateContext(*win);
-  sdlErr(__LINE__);
-
-  SDL_GL_SetSwapInterval(1);
-
-  glewInit();
-}
-
-void draw(SDL_Window **win, GLuint shaderProgram) {
-  SDL_Event event;
-  while (SDL_PollEvent(&event)) {
-    if (event.type == SDL_QUIT)
-      exit(EXIT_SUCCESS);
-
-    if (event.type == SDL_KEYDOWN) {
-      switch (event.key.keysym.sym) {
-      case SDLK_ESCAPE:
-        exit(EXIT_SUCCESS);
-        break;
-      default:
-        break;
-      }
-    }
-  }
-
+void draw() {
   GLfloat vertices[] = {
       -0.5f, -0.5f, 0.0f, // Left
       0.5f,  -0.5f, 0.0f, // Right
@@ -87,37 +36,26 @@ void draw(SDL_Window **win, GLuint shaderProgram) {
   glEnableVertexAttribArray(0);
 
   glDrawArrays(GL_TRIANGLES, 0, 3);
-  SDL_GL_SwapWindow(*win);
+
+  glutSwapBuffers();
 }
 
-int main() {
-
-  SDL_Window *win;
-  SDL_GLContext context;
-  initSDL(&win, &context);
+int main(int argc, char* argv[]) {
+  unsigned int glutDisplayMode = GLUT_RGBA | GLUT_DOUBLE;
+  glutInit(&argc, argv);
+  glutInitWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+  glutInitDisplayMode(glutDisplayMode);
+  glutCreateWindow("diamond");
+  glutReshapeFunc(reshape);
+  glutDisplayFunc(draw);  
+  glewInit();
 
   // load shaders
-  GLuint shaderProgram = load_program("shaders/vs.glsl", "shaders/fs.glsl");
-
-  int w, h;
-  SDL_GL_GetDrawableSize(win, &w, &h);
-  glViewport(0, 0, w, h);
+  shaderProgram = load_program("shaders/vs.glsl", "shaders/fs.glsl");
 
 #ifdef __EMSCRIPTEN__
-  // void emscripten_set_main_loop(em_callback_func func, int fps, int
-  // simulate_infinite_loop);
   emscripten_set_main_loop(draw, 60, 1);
 #else
-  while (1) {
-    draw(&win, shaderProgram);
-    SDL_Delay(200);
-  }
+  glutMainLoop();
 #endif
-
-  // Delete our OpengL context
-  SDL_GL_DeleteContext(context);
-  // Destroy our window
-  SDL_DestroyWindow(win);
-  // Shutdown SDL 2
-  SDL_Quit();
 }
