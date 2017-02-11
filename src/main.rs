5943cf3462449f0681610ce6371d1effaf7de4c1
Javier Maldonado <3goliad@gmail.com>
2017-02-11 23:31:28 +0000

    code is now gfx_app free, also no longer supports anything but opengl

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
-extern crate cgmath;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+extern crate glutin;
 #[macro_use]
 extern crate gfx;
-extern crate gfx_app;
+extern crate gfx_device_gl;
+extern crate gfx_window_glutin;
+extern crate nalgebra;
 extern crate rand;
 extern crate genmesh;
 extern crate noise;
 
-use rand::Rng;
-use cgmath::{SquareMatrix, Matrix4, Point3, Vector3};
-pub use gfx::{Resources};
-pub use gfx::format::{Formatted, DepthStencil};
-pub use gfx_app::{ColorFormat, DepthFormat};
-use genmesh::{Vertices, Triangulate};
-use genmesh::generators::{Plane, SharedVertex, IndexedPolygon};
-use std::time::{Instant};
-use noise::{Seed, perlin2};
+use glutin::{Event, VirtualKeyCode};
+use gfx::Device;
+use gfx::traits::FactoryExt;
+
+use std::time::Instant;
+
+mod engine;
+use engine::Engine;
+mod camera;
+use camera::Camera;
+
+pub type ColorFormat = gfx::format::Rgba8;
+pub type DepthFormat = gfx::format::DepthStencil;
 
 gfx_defines!{
     vertex Vertex {
@@ -34,125 +43,72 @@ gfx_defines!{
         model: gfx::Global<[[f32; 4]; 4]> = "u_Model",
         view: gfx::Global<[[f32; 4]; 4]> = "u_View",
         proj: gfx::Global<[[f32; 4]; 4]> = "u_Proj",
-        out_color: gfx::RenderTarget<ColorFormat> = "Target0",
+      out_color: gfx::RenderTarget<ColorFormat> = "Target0",
         out_depth: gfx::DepthTarget<DepthFormat> =
             gfx::preset::depth::LESS_EQUAL_WRITE,
     }
 }
 
-fn calculate_color(height: f32) -> [f32; 3] {
-    if height > 8.0 {
-        [0.9, 0.9, 0.9] // white
-    } else if height > 0.0 {
-        [0.7, 0.7, 0.7] // greay
-    } else if height > -5.0 {
-        [0.2, 0.7, 0.2] // green
-    } else {
-        [0.2, 0.2, 0.7] // blue
-    }
-}
-
-struct Diamond<R: gfx::Resources> {
-    pso: gfx::PipelineState<R, pipe::Meta>,
-    data: pipe::Data<R>,
-    slice: gfx::Slice<R>,
-    start_time: Instant,
-}
-
-//backend: gfx_app::shade::Backend, window_targets: gfx_app::WindowTargets<R>
-impl <R: gfx::Resources> gfx_app::Application<R> for Diamond<R> {
-    fn new<F: gfx::Factory<R>>(mut factory: F, init: gfx_app::Init<R>) -> Self {
-        use gfx::traits::FactoryExt;
-
-        let vs = gfx_app::shade::Source {
-            glsl_120: include_bytes!("shader/terrain_120.glslv"),
-            glsl_150: include_bytes!("shader/terrain_150.glslv"),
-            .. gfx_app::shade::Source::empty()
-        };
-        let ps = gfx_app::shade::Source {
-            glsl_120: include_bytes!("shader/terrain_120.glslf"),
-            glsl_150: include_bytes!("shader/terrain_150.glslf"),
-            .. gfx_app::shade::Source::empty()
-        };
-
-        let rand_seed = rand::thread_rng().gen();
-        let seed = Seed::new(rand_seed);
-        let plane = Plane::subdivide(256, 256);
-        let vertex_data: Vec<Vertex> = plane.shared_vertex_iter()
-            .map(|(x, y)| {
-                let h = perlin2(&seed, &[x, y]) * 32.0;
-                Vertex {
-                    pos: [25.0 * x, 25.0 * y, h],
-                    color: calculate_color(h),
-                }
-            })
-            .collect();
-
-        let index_data: Vec<u32> = plane.indexed_polygon_iter()
-            .triangulate()
-            .vertices()
-            .map(|i| i as u32)
-            .collect();
-
-        let (vbuf, slice) = factory.create_vertex_buffer_with_slice(&vertex_data, &index_data[..]);
-
-        Diamond {
-            pso: factory.create_pipeline_simple(
-                vs.select(init.backend).unwrap(),
-                ps.select(init.backend).unwrap(),
-                pipe::new()
-                ).unwrap(),
-            data: pipe::Data {
-                vbuf: vbuf,
-                locals: factory.create_constant_buffer(1),
-                model: Matrix4::identity().into(),
-                view: Matrix4::identity().into(),
-                proj: cgmath::perspective(
-                    cgmath::Deg(60.0f32), init.aspect_ratio, 0.1, 1000.0
-                    ).into(),
-                out_color: init.color,
-                out_depth: init.depth,
-            },
-            slice: slice,
-            start_time: Instant::now(),
-        }
-    }
-
-    fn render<C: gfx::CommandBuffer<R>>(&mut self, encoder: &mut gfx::Encoder<R, C>) {
-        let elapsed = self.start_time.elapsed();
-        let time = elapsed.as_secs() as f32 + elapsed.subsec_nanos() as f32 / 1000_000_000.0;
-        let x = time.sin();
-        let y = time.cos();
-        let view = Matrix4::look_at(
-            Point3::new(x * 32.0, y * 32.0, 16.0),
-            Point3::new(0.0, 0.0, 0.0),
-            Vector3::unit_z(),
-        );
-
-        self.data.view = view.into();
-        let locals = Locals {
-            model: self.data.model,
-            view: self.data.view,
-            proj: self.data.proj,
-        };
-
-        encoder.update_buffer(&self.data.locals, &[locals], 0).unwrap();
-        encoder.clear(&self.data.out_color, [0.3, 0.3, 0.3, 1.0]);
-        encoder.clear_depth(&self.data.out_depth, 1.0);
-        encoder.draw(&self.slice, &self.pso, &self.data);
-    }
-
-    // this is important but i don't know where it goes
-    //fn on_resize(&mut self, window_targets: gfx_app::WindowTargets<R>) {
-    //    self.data.out_color = window_targets.color;
-    //    self.data.out_depth = window_targets.depth;
-    //    self.data.proj = cgmath::perspective(
-    //        cgmath::deg(60.0f32), window_targets.aspect_ratio, 0.1, 1000.0
-    //        ).into();
-    //}
-}
-
 pub fn main() {
-    use gfx_app::Application;
-    Diamond::launch_default("diamond");
+    env_logger::init().unwrap();
+    let mut camera = Camera::new();
+    let engine = Engine::new();
+    let builder = glutin::WindowBuilder::new()
+        .with_title("diamond".to_string())
+        .with_vsync();
+    let (window, mut device, mut factory, main_color, main_depth) =
+        gfx_window_glutin::init::<ColorFormat, DepthFormat>(builder);
+    let mut encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
+    let (screen_width, screen_height) = window.get_inner_size_points().unwrap();
+    camera.set_aspect_ratio(screen_width as f32 / screen_height as f32);
+    let pso = factory.create_pipeline_simple(include_bytes!("shader/terrain.glslv"),
+                                include_bytes!("shader/terrain.glslf"),
+                                pipe::new())
+        .unwrap();
+    let (vertex_data, index_data) = engine.to_vertex();
+    let (vbuf, slice) = factory.create_vertex_buffer_with_slice(&vertex_data, index_data.as_slice());
+    let mut data = pipe::Data {
+        vbuf: vbuf,
+        locals: factory.create_constant_buffer(1),
+        model: camera::identity_matrix(),
+        view: camera.view(),
+        proj: camera.perspective(),
+        out_color: main_color,
+        out_depth: main_depth,
+    };
+    let start_time = Instant::now();
+    'main: loop {
+        for event in window.poll_events() {
+            match event {
+                Event::Closed | Event::KeyboardInput(_, _, Some(VirtualKeyCode::Escape)) => break 'main,
+                Event::Resized(width, height) => {
+                    camera.set_aspect_ratio(width as f32 / height as f32);
+                    data.proj = camera.perspective();
+                    gfx_window_glutin::update_views(&window,
+                                                    &mut data.out_color,
+                                                    &mut data.out_depth);
+                },
+                _ => {},
+            }
+        }
+        // draw a frame
+        let elapsed = start_time.elapsed();
+        let time = elapsed.as_secs() as f32 + elapsed.subsec_nanos() as f32 / 1000_000_000.0;
+        camera.move_to(&[time.sin() * 32.0, time.cos() * 32.0, 32.0]);
+        data.view = camera.view();
+
+        let locals = Locals {
+            model: data.model,
+            view: data.view,
+            proj: data.proj,
+        };
+
+        encoder.update_buffer(&data.locals, &[locals], 0).unwrap();
+        encoder.clear(&data.out_color, [0.1, 0.2, 0.3, 1.0]);
+        encoder.clear_depth(&data.out_depth, 1.0);
+        encoder.draw(&slice, &pso, &data);
+        encoder.flush(&mut device);
+        window.swap_buffers().unwrap();
+        device.cleanup();
+    }
 }
